e535f336f6222a6f86e357d5455cd3d914593d2c
Jusety <seva.venedikt0v@yandex.ru>
2023-08-14 01:39:29 +0300

    #

--- src/Components/UI/navbar/Navbar.jsx
+++ src/Components/UI/navbar/Navbar.jsx
@@ -20,11 +20,23 @@ const Navbar = () => {
                     </MyButton>
                 )}
                 <div className="navbar__links">
-                    <Link className="link" to="/about">
+                    <Link
+                        className="link"
+                        to="/about"
+                        style={{
+                            marginLeft: 100,
+                            position: "relative",
+                            top: "20px",
+                        }}
+                    >
                         About us
                     </Link>{" "}
                     <br />
-                    <Link className="link" to="/posts">
+                    <Link
+                        className="link"
+                        to="/posts"
+                        style={{ position: "relative", bottom: "22px" }}
+                    >
                         Posts
                     </Link>
                 </div>
